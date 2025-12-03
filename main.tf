257cce926e5cfe0dfab9b84b91ee398a4f0e3c12
Ubuntu <ubuntu@ip-172-31-5-12.ap-south-1.compute.internal>
2025-12-03 10:59:02 +0000

    created ec2 instance in aws using terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-02b8269d5e85954ef"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform_Example"
+  }
+}
